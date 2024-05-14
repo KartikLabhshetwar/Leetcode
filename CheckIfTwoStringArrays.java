19fd044a9a0d90c21d18a0598c8077ac6376d36a
Kartik Labhshetwar <141413343+KartikLabhshetwar@users.noreply.github.com>
2024-05-14 14:52:14 +0530

    Create CheckIfTwoStringArrays.java

---
+++ CheckIfTwoStringArrays.java
@@ -0,0 +1,27 @@
+/*
+1662. Check If Two String Arrays are Equivalent
+
+Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
+A string is represented by an array if the array elements concatenated in order forms the string.
+*/
+
+class CheckIfTwoStringArrays {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+
+        for (int i = 0; i < word1.length; i++) {
+            sb1.append(word1[i]);
+        }
+
+        for (int i = 0; i < word2.length; i++) {
+            sb2.append(word2[i]);
+        }
+
+        if (sb1.toString().equals(sb2.toString())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
